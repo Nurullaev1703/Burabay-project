7c0b92defb0f5175000ef684dd68d5e911f9a3dc
duckn1ght <duckn1ght04@gmail.com>
2024-12-17 20:31:32 +0500

    static content

--- burabay-server/src/app.module.ts
+++ burabay-server/src/app.module.ts
@@ -19,9 +19,15 @@ import { ScheduleModule } from './schedule/schedule.module';
 import { BreaksModule } from './breaks/breaks.module';
 import { AddressModule } from './address/address.module';
 import { BookingBanDateModule } from './booking-ban-date/booking-ban-date.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', `public`),
+      serveRoot: '/',
+    }),
     MainPageModule,
     ConfigModule.forRoot({ isGlobal: true }),
     ThrottlerModule.forRoot([
