c1e380b043b33c061ce68bcbe461eed82b16d7b9
Duman <kaltaev.duman0705@gmail.com>
2024-12-25 23:31:10 +0500

    Announcement -> Schedule page

--- burabay-client/src/pages/announcements/announcement/ui/AnnouncementInfoList.tsx
+++ burabay-client/src/pages/announcements/announcement/ui/AnnouncementInfoList.tsx
@@ -1,17 +1,19 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { AnnouncementSchedule } from "../../model/announcements";
+import { Schedule } from "../../model/announcements";
 import { COLORS_TEXT } from "../../../../shared/ui/colors";
 import ArrowRight from "../../../../app/icons/arrow-right.svg";
 import { Link } from "@tanstack/react-router";
 interface Props {
   phoneNumber: string;
-  schedule: AnnouncementSchedule | null | string;
+  schedule: Schedule | null | string;
+  id: string;
 }
 
 export const AnnouncementInfoList: FC<Props> = function AnnouncementInfoList({
   phoneNumber,
   schedule,
+  id
 }) {
   const { t } = useTranslation();
 
@@ -20,7 +22,7 @@ export const AnnouncementInfoList: FC<Props> = function AnnouncementInfoList({
     if (!schedule || typeof schedule === "string") {
       return "Нет расписания";
     }
-
+  
     const daysMap: { [key: string]: string } = {
       mon: "Пн",
       tue: "Вт",
@@ -30,23 +32,24 @@ export const AnnouncementInfoList: FC<Props> = function AnnouncementInfoList({
       sat: "Сб",
       sun: "Вс",
     };
-
-    // Возврат Пн, Вт и тд
+  
+    // Фильтрация дней с рабочим временем не равным "00:00"
     const workingDays = Object.entries(daysMap)
       .filter(([key]) => {
         const startKey = `${key}Start`;
         const endKey = `${key}End`;
         return (
-          schedule[startKey as keyof AnnouncementSchedule] &&
-          schedule[endKey as keyof AnnouncementSchedule]
+          schedule[startKey as keyof Schedule] !== "00:00" &&
+          schedule[endKey as keyof Schedule] !== "00:00"
         );
       })
       .map(([, value]) => value);
-
+  
     return workingDays.length > 0
-      ? `${workingDays.join(", ")}`
+      ? workingDays.join(", ")
       : "Нет рабочих дней";
   };
+  
 
   const formatPhoneNumber = (number: number | string) => {
     const phoneStr = number.toString().replace(/\D/g, ""); // Убираем все нецифровые символы
@@ -62,7 +65,7 @@ export const AnnouncementInfoList: FC<Props> = function AnnouncementInfoList({
         </span>
       </li>
       <li className="border-b border-[#999999] py-3">
-        <Link to={`/announcements/schedule`} className="flex justify-between">
+        <Link to={`/announcements/schedule/${id}`} className="flex justify-between">
           <div className="flex flex-col">
             <span>{renderSchedule()}</span>
             <span className={`${COLORS_TEXT.gray100} text-sm`}>
