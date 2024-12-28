2641424d88739efb01eab18574c8780ff37e3239
duckn1ght <duckn1ght04@gmail.com>
2024-12-29 01:33:49 +0500

    nginx for SSL

--- burabay-client/vite.config.ts
+++ burabay-client/vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  server: {
-    host: true,
-    port: 80,
-    open: true,
-    hmr: true,
-    proxy: {
-      "/api": "http://91.215.139.89:3000", // Прокси для API-запросов
+    plugins: [react(), TanStackRouterVite()],
+    server: {
+        host: true,
+        port: 5173,
+        open: true,
+        hmr: true,
+        proxy: {
+            "/api": "http://91.215.139.89:3000", // Прокси для API-запросов
+        },
     },
-  }
 });
