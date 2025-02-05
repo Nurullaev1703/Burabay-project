85e005ceb3418c71d86494ea02a2558b4e58465d
zhenya <zalixdan@mail.ru>
2025-02-05 14:08:56 +0500

    noti

--- burabay-client/src/routes/notifications/notificationsOrg/index.tsx
+++ burabay-client/src/routes/notifications/notificationsOrg/index.tsx
@@ -1,7 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Notifications } from '../../../pages/notifications/notificationOrg.tsx/Notifications'
+import { Loader } from '../../../components/Loader';
+import { useGetNotification } from '../../../pages/notifications/notificationOrg/notificaions-utils';
+import { Notifications } from '../../../pages/notifications/notificationOrg/Notifications';
 
 export const Route = createFileRoute('/notifications/notificationsOrg/')({
-  component: Notifications,
+  component: GetNotification,
 })
 
+function GetNotification() {
+  const { data, isLoading } = useGetNotification();
+    if (isLoading) {
+      return <Loader />;
+    }
+    if (data) {
+      return <Notifications notifications={data} />;
+    }
+}
