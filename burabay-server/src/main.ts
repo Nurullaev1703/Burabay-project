ee75c00bc9f10b75d5ee253e8d15121ea15a1139
duckn1ght <duckn1ght04@gmail.com>
2025-02-19 15:05:55 +0500

    test server setup

--- burabay-server/src/main.ts
+++ burabay-server/src/main.ts
@@ -26,11 +26,13 @@ async function bootstrap() {
   app.enableCors({
     origin: (origin, callback) => {
       const allowedOrigins = [
-        'http://localhost:3000',
+        'http://localhost:3001',
         'http://localhost:5174',
         'http://localhost:5173',
         'http://91.215.139.89:5173',
+        'http://91.215.139.89:5174',
         'http://192.168.100.222:5173',
+        'http://192.168.100.222:5174',
       ];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
@@ -49,6 +51,6 @@ async function bootstrap() {
     next();
   });
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
