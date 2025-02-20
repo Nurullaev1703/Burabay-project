cb4f8c7d15bd502dc5813a124e9ba575dfd45bfc
Nurullaev Ilya <nurullaev567@gmail.com>
2025-02-20 12:34:12 +0500

    update front api routes

--- burabay-client/vite.config.ts
+++ burabay-client/vite.config.ts
@@ -4,17 +4,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
-    server: {
-        host: true,
-        port: 5174,
-        open: true,
-        hmr: true,
-        proxy: {
-            "/api": "http://localhost:3000", // Прокси для API-запросов
-        },
+  plugins: [react(), TanStackRouterVite()],
+  server: {
+    host: true,
+    port: 5174,
+    open: true,
+    hmr: true,
+    proxy: {
+      "/api": "https://burabay-damu.kz/back", // Прокси для API-запросов
     },
-    preview: {
-        port: 80,
-    },
+  }
 });
