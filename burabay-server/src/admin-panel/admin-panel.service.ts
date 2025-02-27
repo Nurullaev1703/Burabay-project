d6d34f78bc39f2ed377bafc3fad0a60a5bb76790
duckn1ght <duckn1ght04@gmail.com>
2025-02-27 22:43:11 +0500

    add org id

--- burabay-server/src/admin-panel/admin-panel.service.ts
+++ burabay-server/src/admin-panel/admin-panel.service.ts
@@ -108,6 +108,7 @@ export class AdminPanelService {
         adName: review.ad.title,
         adReviewCount: review.ad.reviewCount,
         adRating: review.ad.avgRating,
+        orgId: review.ad.organization.id,
         orgName: review.ad.organization.name,
         orgImage: review.ad.organization.imgUrl,
         reportText: review.report.text,
