c784358609fc3001f83af184670e05a705a1e9b7
duckn1ght <duckn1ght04@gmail.com>
2025-04-15 14:06:28 +0500

    get all reviews

--- burabay-server/src/main.ts
+++ burabay-server/src/main.ts
@@ -1,26 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Burabay')
-  //   .setDescription('server configuration')
-  //   .setVersion('1.0')
-  //   .addBearerAuth()
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
-
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('pug');
+  const config = new DocumentBuilder()
+    .setTitle('Burabay')
+    .setDescription('server configuration')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   // Включение CORS
   app.enableCors({
