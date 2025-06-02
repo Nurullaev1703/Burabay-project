71eaa7cdd2a297cd48c7c72fc2b664e5017dc944
Ilya Nurullaev <nurullaev567@gmail.com>
2025-06-02 17:21:32 +0500

    update admin message panel

--- burabay-client/src/pages/admin/dashboard/MessagesPage.tsx
+++ burabay-client/src/pages/admin/dashboard/MessagesPage.tsx
@@ -172,13 +172,11 @@ const MessagesPage: FC<Props> = ({ categories }) => {
         className="absolute inset-0 bg-cover bg-center opacity-25"
         style={{ backgroundImage: `url(${authBg})` }}
       ></div>
-
       <div className="relative z-50">
         <SideNav />
-      </div>
-
-      <div className="relative z-10 flex flex-col w-full p-4 ml-[94px]">
-        <div className="flex-1 overflow-y-auto max-h-[calc(100vh-120px)] pt-0">
+      </div>{" "}
+      <div className="relative z-10 flex flex-col w-full p-4 ml-[94px] h-screen">
+        <div className="flex-1 overflow-y-auto pb-[140px]">
           {loading ? (
             <Loader />
           ) : (
@@ -215,124 +213,140 @@ const MessagesPage: FC<Props> = ({ categories }) => {
           <div ref={messagesEndRef} />
         </div>
 
-        {selectedRole.toLowerCase() == "турист" && (
-          <div className="sticky px-4 bottom-[120px] left-0 flex justify-start max-w-[1400px] overflow-x-scroll gap-2 ">
-            {categories.map((item) => {
-              return (
+        {/* Fixed bottom panel */}
+        <div className="fixed bottom-0 left-[94px] right-0 z-20 backdrop-blur-lg px-4 py-2">
+          {selectedRole.toLowerCase() == "турист" && (
+            <div className="w-full pb-2">
+              <div className="flex flex-wrap gap-2 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent max-h-[80px]">
+                {categories.map((item) => {
+                  return (
+                    <button
+                      type="button"
+                      key={item.id}
+                      onClick={() => {
+                        if (categoryNames?.includes(item)) {
+                          setCategoryNames(
+                            categoryNames.filter(
+                              (category) => category.name !== item.name
+                            )
+                          );
+                        } else {
+                          setCategoryNames([...categoryNames, item]);
+                        }
+                      }}
+                      className={`
+                flex-shrink-0 w-fit
+                rounded-full justify-between flex items-center p-1 pr-4 gap-2 ${categoryNames?.includes(item) ? categoryBgColors[item.name] : "bg-white"} `}
+                    >
+                      <div
+                        className={`relative min-w-7 min-h-7 rounded-full ${categoryBgColors[item.name]}`}
+                      >
+                        <img
+                          src={baseUrl + item.imgPath}
+                          className="absolute top-1/2 left-1/2 w-4 h-4 mr-2 -translate-x-1/2 -translate-y-1/2 brightness-[25] z-[0]"
+                        />
+                      </div>
+                      <Typography
+                        size={16}
+                        weight={400}
+                        color={
+                          categoryNames?.includes(item) ? COLORS_TEXT.white : ""
+                        }
+                        className={`text-center line-clamp-1 whitespace-nowrap`}
+                      >
+                        {item.name}
+                      </Typography>
+
+                      {categoryNames?.includes(item) && (
+                        <div className="w-3">
+                          <img src={cancel} alt="Close" className="" />
+                        </div>
+                      )}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+
+          <div className="w-full p-4 rounded-xl bg-white">
+            <div className="flex flex-col sm:flex-row items-stretch sm:items-center gap-4">
+              <div className="relative flex-shrink-0 w-full sm:w-[256px]">
                 <button
                   type="button"
-                  key={item.id}
-                  onClick={() => {
-                    if (categoryNames?.includes(item)) {
-                      setCategoryNames(
-                        categoryNames.filter(
-                          (category) => category.name !== item.name
-                        )
-                      );
-                    } else {
-                      setCategoryNames([...categoryNames, item]);
-                    }
-                  }}
-                  className={`
-                     w-fit
-                    rounded-full justify-between  flex  items-center p-1 pr-4 gap-2 ${categoryNames?.includes(item) ? categoryBgColors[item.name] : "bg-white"} `}
+                  className="flex items-center justify-between text-[#0A7D9E] font-roboto py-3 px-4 border-[1px] rounded-[8px] border-[#0A7D9E] bg-white w-full"
+                  onClick={() => setIsSelect(!isSelect)}
                 >
-                  <div
-                    className={`relative min-w-7 min-h-7 rounded-full ${categoryBgColors[item.name]}  `}
-                  >
-                    <img
-                      src={baseUrl + item.imgPath}
-                      className="absolute top-1/2 left-1/2 w-4 h-4 mr-2 -translate-x-1/2 -translate-y-1/2 brightness-[25] z-[0]"
-                    />
-                  </div>
-                  <Typography
-                    size={16}
-                    weight={400}
-                    color={
-                      categoryNames?.includes(item) ? COLORS_TEXT.white : ""
-                    }
-                    className={`text-center line-clamp-1`}
-                  >
-                    {item.name}
-                  </Typography>
-
-                  {categoryNames?.includes(item) && (
-                    <div className="w-3">
-                      <img src={cancel} alt="Close" className="" />
-                    </div>
-                  )}
-                </button>
-              );
-            })}
-          </div>
-        )}
-        <div className="sticky bottom-0 left-0 right-0 flex items-center max-w-[1400px] p-4 bg-white backdrop-blur-lg rounded-xl">
-          <div className="relative mr-4 w-[256px]">
-            <button
-              type="button"
-              className="flex items-center justify-between text-[#0A7D9E] font-roboto py-3 px-4 border-[1px] rounded-[8px] border-[#0A7D9E] bg-white w-[240px]"
-              onClick={() => setIsSelect(!isSelect)}
-            >
-              <img src={UsersIcon} className="min-w-6 min-h-6 mr-4" />
-              <span className="capitalize">{selectedRole}</span>
-              <img src={Down} alt="" className="ml-4 w-[16px] h-[16px]" />
-            </button>
-            {isSelect && (
-              <div className="absolute left-0 bottom-16 mt-1  bg-white morph rounded shadow-md z-10 border">
-                <label className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  <input
-                    type="radio"
-                    name="roleFilter"
-                    className="mr-2 h-5 w-5 accent-[#0A7D9E] cursor-pointer"
-                    checked={selectedRole === "Все пользователи"}
-                    onChange={() => {
-                      setSelectedRole("Все пользователи");
-                      setIsSelect(false);
-                    }}
+                  <img src={UsersIcon} className="min-w-6 min-h-6 mr-4" />
+                  <span className="capitalize flex-1 text-left">
+                    {selectedRole}
+                  </span>
+                  <img
+                    src={Down}
+                    alt=""
+                    className="ml-4 w-[16px] h-[16px] flex-shrink-0"
                   />
-                  {"Все пользователи"}
-                </label>
-                {Object.values(RoleType)
-                  .filter((roleValue) => roleValue !== "admin")
-                  .map((roleValue) => (
-                    <label
-                      key={roleValue}
-                      className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer capitalize"
-                    >
+                </button>
+                {isSelect && (
+                  <div className="absolute left-0 bottom-16 mt-1 bg-white morph rounded shadow-md z-30 border w-full min-w-[256px]">
+                    <label className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
                       <input
                         type="radio"
                         name="roleFilter"
-                        value={roleValue}
                         className="mr-2 h-5 w-5 accent-[#0A7D9E] cursor-pointer"
-                        checked={selectedRole === roleValue}
+                        checked={selectedRole === "Все пользователи"}
                         onChange={() => {
-                          setSelectedRole(roleValue);
+                          setSelectedRole("Все пользователи");
                           setIsSelect(false);
                         }}
                       />
-                      {roleValue}
+                      {"Все пользователи"}
                     </label>
-                  ))}
+                    {Object.values(RoleType)
+                      .filter((roleValue) => roleValue !== "admin")
+                      .map((roleValue) => (
+                        <label
+                          key={roleValue}
+                          className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer capitalize"
+                        >
+                          <input
+                            type="radio"
+                            name="roleFilter"
+                            value={roleValue}
+                            className="mr-2 h-5 w-5 accent-[#0A7D9E] cursor-pointer"
+                            checked={selectedRole === roleValue}
+                            onChange={() => {
+                              setSelectedRole(roleValue);
+                              setIsSelect(false);
+                            }}
+                          />
+                          {roleValue}
+                        </label>
+                      ))}
+                  </div>
+                )}
               </div>
-            )}
+              <div className="flex flex-1 items-center gap-3 ">
+                <input
+                  type="text"
+                  className="flex-1 min-w-0 px-4 py-3 border border-[#EDECEA] rounded-lg bg-[#FAF9F7] focus:border-blue200 focus:outline-none"
+                  placeholder="Введите уведомление..."
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                />
+                <button
+                  className="w-[52px] h-[52px] flex-shrink-0 rounded-full bg-[#0A7D9E] transition bg-cover bg-center"
+                  onClick={handleSendNotification}
+                  style={{
+                    backgroundImage: `url(${message})`,
+                    backgroundSize: "22px 22px",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                  }}
+                />
+              </div>
+            </div>
           </div>
-          <input
-            type="text"
-            className="flex-1 w-[100%] px-4 py-3 border border-[#EDECEA] rounded-lg bg-[#FAF9F7] focus:border-blue200 focus:outline-none"
-            placeholder="Введите уведомление..."
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-          />
-          <button
-            className="w-[52px] h-[52px] rounded-full bg-[#0A7D9E] transition bg-cover bg-center ml-3"
-            onClick={handleSendNotification}
-            style={{
-              backgroundImage: `url(${message})`,
-              backgroundSize: "22px 22px",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-          />
         </div>
       </div>
     </div>
