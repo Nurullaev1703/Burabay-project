19b4efec94c9b76b65b4576d1a0cf4cfc496bdeb
Nurullaev Ilya <nurullaev567@gmail.com>
2025-11-07 19:42:35 +0500

    feat: обновить интерфейс авторизации и добавить обработку ошибок для администратора

--- burabay-client/src/components/admin/SideNav.tsx
+++ burabay-client/src/components/admin/SideNav.tsx
@@ -75,7 +75,7 @@ const SideNav: React.FC<SideNavProps> = ({ className }) => {
   return (
     <div
       className={`side-nav ${className} overflow-hidden fixed top-0 left-0 h-full bg-[#0A7D9E]  px-4 py-8
-      transition-all duration-300 ease-linear z-50
+      transition-all duration-300 ease-linear z-50 rounded-tr-lg rounded-br-2xl
       ${isExpanded ? "w-[312px]" : "w-[94px] "} 
       `}
       onMouseEnter={() => setIsExpanded(true)}
