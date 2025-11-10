d2c4876a1e2885813c1e5a28ee7b06126118a862
duckn1ght <duckn1ght04@gmail.com>
2025-11-10 12:11:51 +0500

    Update booking.service.ts

--- burabay-server/src/booking/booking.service.ts
+++ burabay-server/src/booking/booking.service.ts
@@ -615,6 +615,7 @@ export class BookingService {
   async cancelExpiredUnacceptedBookings() {
     const now = new Date();
     const expiredBookings = await this.bookingRepository.find({
+      relations: { user: true },
       where: {
         status: BookingStatus.IN_PROCESS,
         dateEnd: LessThanOrEqual(now),
