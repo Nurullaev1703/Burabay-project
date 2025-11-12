84ff5e375da7833811b84fe98093d18f8e2782c3
zhenya <zalixdan@mail.ru>
2025-11-12 14:06:34 +0500

    fix

--- burabay-client/src/pages/mapNav/MapNav.tsx
+++ burabay-client/src/pages/mapNav/MapNav.tsx
@@ -703,6 +703,27 @@ export const MapNav: FC<Props> = ({ announcements, categories, filters }) => {
             onClick={(e) => e.stopPropagation()}
           >
             <div className="w-full">
+              <button
+                onClick={handleCloseModal}
+                className="absolute top-4 right-4 z-10"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M18 6L6 18M6 6L18 18"
+                    stroke="#0a7d9e
+"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
               <div className="flex flex-col gap-4 w-full">
                 <div className="flex flex-col w-full min-w-0">
                   <Typography
