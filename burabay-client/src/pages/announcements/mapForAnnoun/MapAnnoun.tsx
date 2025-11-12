4766233f26b0bb066ca5b39ce7651a2f93d2e563
zhenya <zalixdan@mail.ru>
2025-11-12 14:10:04 +0500

    fix

--- burabay-client/src/pages/announcements/mapForAnnoun/MapAnnoun.tsx
+++ burabay-client/src/pages/announcements/mapForAnnoun/MapAnnoun.tsx
@@ -536,6 +536,26 @@ export const MapAnnoun: FC<Props> = ({ announcements }) => {
             onClick={(e) => e.stopPropagation()}
           >
             <div className="w-full">
+              <button
+                onClick={handleCloseModal}
+                className="absolute top-4 right-4 z-10"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M18 6L6 18M6 6L18 18"
+                    stroke="#0a7d9e"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
               <div className="flex flex-col gap-4 w-full">
                 <div className="flex flex-col w-full min-w-0">
                   <Typography
