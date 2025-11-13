eaf4ae28afabb6903a232bdd07c3234f98b200d5
zhenya <zalixdan@mail.ru>
2025-11-13 14:29:27 +0500

    fix

--- burabay-client/src/pages/announcements/booking/Booking.tsx
+++ burabay-client/src/pages/announcements/booking/Booking.tsx
@@ -128,15 +128,15 @@ export const Booking: FC = function Booking() {
       </Header>
 
       <div className="mb-4 px-4">
-        <div className="flex">
+        <div className="flex min-w-0 mt-4">
           <img
             src={imageSrc}
             onError={() => setImageSrc(DefaultIcon)}
             alt={announcement.title}
             className="w-[52px] h-[52px] object-cover rounded-lg mr-2"
           />
-          <div>
-            <span>{announcement.title}</span>
+          <div className="flex-1 min-w-0">
+            <span className="block font-medium truncate max-w-full">{announcement.title}</span>
             <div className="flex items-center">
               <div className="flex items-center mr-2">
                 <img src={StarIcon} className="w-[16px] mr-1 mb-1" />
