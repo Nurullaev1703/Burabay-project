18b83db4ad4e8044fdf61888a3ae30ce8de3720a
zhenya <zalixdan@mail.ru>
2026-01-15 15:25:48 +0500

    fixicon

--- burabay-client/src/components/OfflineScreen.tsx
+++ burabay-client/src/components/OfflineScreen.tsx
@@ -21,11 +21,11 @@ const OfflineScreen: FC<Props> = ({ onRetry }) => {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M2 8.82C6.5 5 11.5 5 16 8.82" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M2 8.82C6.5 5 17 5 21.5 8.82C6.5" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M5.5 12.78C8.2 10.28 15.8 10.28 18.5 12.78" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M8.5 16.74C10.1 15.2 13.9 15.2 15.5 16.74" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M12 20.5C12.8284 20.5 13.5 19.8284 13.5 19C13.5 18.1716 12.8284 17.5 12 17.5C11.1716 17.5 10.5 18.1716 10.5 19C10.5 19.8284 11.1716 20.5 12 20.5Z" fill="currentColor"/>
-            <path d="M3 3L21 21" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M1 3L22 18" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </div>
 
